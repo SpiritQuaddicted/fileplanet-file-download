a349c758f6fe3829cd52c96386134d3afaabe328
Spirit <spirit@quaddicted.com>
2012-05-24 19:24:57 +0200

    Added a referrer since fileplanet wants that now. Also reworked some minor things. Wiki code output.

--- download_pages_and_files_from_fileplanet.sh
+++ download_pages_and_files_from_fileplanet.sh
@@ -16,28 +16,30 @@
 echo "You will be downloading $1 to $2, you rock!"
 echo "Let's go!"
 
+mkdir logs
 mkdir "$1-$2"
 cd $1-$2/
 
 for i in $(seq $1 $2)
 do
 	echo "Trying to download $i"
+	downloadpageurl="www.fileplanet.com/${i}/download/"
 	
 	# fileplanet returns a "302 Found" for non-existing IDs
 	# redirecting to "Location: /error/error.shtml?aspxerrorpath=/autodownload.aspx 
 	# we don't want those files, so "--max-redirect=0"
-	wget -nv -a pages_$1_$2.log --force-directories --max-redirect=0 http://www.fileplanet.com/${i}/download/
+	wget -nv -a pages_$1_$2.log --force-directories --max-redirect=0 http://${downloadpageurl}
 	
 	# extract the session download link to the actual file we want
 	# the URL is enclosed by single quotes. The second grep will get everything from http until the last '. The rev/cut will remove the trailing '.  
-	linktowget=$(grep default-file-download-link www.fileplanet.com/${i}/download/index.html 2>/dev/null | grep -Eo "http.*'" | rev | cut -c 2- | rev)
+	linktowget=$(grep default-file-download-link ${downloadpageurl}index.html 2>/dev/null | grep -Eo "http.*'" | rev | cut -c 2- | rev)
 	
 	if [ ! -n "${linktowget}" ]; then
 		echo "No download link found."
 	else
 		echo "Download link found, downloading ${linktowget}"
 		# download the file to the same directory as its download page HTML
-		wget -nv -a files_$1_$2.log --directory-prefix=www.fileplanet.com/${i}/download/ "${linktowget}"
+		wget -nv -a files_$1_$2.log --directory-prefix=${downloadpageurl} --referer="${downloadpageurl}" "${linktowget}" || echo "ERROR! If you see more than one, please Ctrl-C, check the files log and tell Schbirid!"
 	fi
 	echo "-----"
 done
@@ -45,21 +47,29 @@ done
 echo "Downloading finished! Yay!"
 
 echo -n "Counting files: "
-ls -1 www.fileplanet.com/ | wc -l
+numberoffiles=$(ls -1 www.fileplanet.com/ | wc -l)
+echo $numberoffiles
 
 echo -n "Getting the size: "
-du -hs www.fileplanet.com/
+sizeofchunk=$(du -hs www.fileplanet.com/ | sed 's/\twww.*//')
+echo $sizeofchunk
 
-echo "TARring!"
 cd ..
-tar -cf $1-$2.tar $1-$2/
 
 # just a handy local backup
 cp $1-$2/*.log logs/
 
-# This is not recommended to do automatically. The chunk might have been tiny or huge. Better check first.
-# echo "Uploading to archive.org!"
-# s3cmd --add-header x-archive-auto-make-bucket:1 --add-header "x-archive-meta-description:Files from Fileplanet (www.fileplanet.com), all files from the ID range $1 to $2." put $1-$2.tar s3://FileplanetFiles_$1-$2
-# s3cmd put $1-$2/*.log s3://FileplanetFiles_$1-$2/
+# Uncomment the following lines if you want the script to tar automatically. Only use this if you have enough free space
+# Uncomment the end of the tar line to make it remove the directory if tar was successful
+#echo "TARring!"
+#tar -cf $1-$2.tar $1-$2/ # && rm -r $1-$2/
 
-echo "Done. YAAAY!"
+grep "ERROR" logs/*$1_$2.log || echo "Done. YAAAY!"
+echo "Here is copy'n'pastable meta for the wiki:"
+echo "|-"
+echo "| $1-$2"
+echo "| Done, locally"
+echo "| $numberoffiles"
+echo "| $sizeofchunk"
+echo "| insert nick name here"
+echo "|-"
